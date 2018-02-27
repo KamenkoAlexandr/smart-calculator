8caf847f2231745d60eebbe83bc54a3814da75cc
Kamenko <a.kamenko98@mail.ru>
2018-02-27 22:34:09 +0300

    Finally Calculator

--- src/index.js
+++ src/index.js
@@ -1,27 +1,56 @@
 class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+    constructor(initialValue) {
+        this.array = [initialValue];
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    toString (){
+        for (var i = 0; i < this.array.length; i++) {
+            if (this.array[i] === "^") {
+                var k = 2, j = i+2, helpStr = [];
+                helpStr.push(this.array[i-1], this.array[i+1]);
+                while (this.array[j] === "^" ) {
+                    helpStr.push(this.array[j+1]);
+                    j+=2;
+                    k=k+2;
+                }
+                this.array.splice(i,k);
+                for (var p = helpStr.length-1; p > 0; p--) {
+                    helpStr[p-1] = Math.pow(helpStr[p-1], helpStr[p]);
+                    helpStr.pop();
+                }
+                this.array[i-1] = helpStr[0];
+                i--;
+            }
+        }
+        var x = this.array.join("");
+        return eval(x);
+    }
+    add(number) {
+        this.array.push('+', number);
+        return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    subtract(number) {
+        this.array.push('-', number);
+        return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    multiply(number) {
+        this.array.push('*', number);
+        return this;
 
-  pow(number) {
-    // your implementation
-  }
+    }
+
+    devide(number) {
+        this.array.push('/', number);
+        return this;
+
+    }
+
+    pow(number) {
+        this.array.push('^', number);
+        return this;
+    }
 }
 
 module.exports = SmartCalculator;
